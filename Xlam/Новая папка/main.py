d45f96ed1a9636dc259d2f437af4ba532d384461
VadimChubar <105510927+VadimChubar@users.noreply.github.com>
2022-11-06 11:44:56 +0200

    Update main.py

--- Xlam/Новая папка/main.py
+++ Xlam/Новая папка/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import argparse
 from pathlib import Path
@@ -60,3 +61,67 @@ if __name__ == '__main__':
 
     print('Можно удалять стару папку якщо треба')
 
+=======
+
+import argparse
+from pathlib import Path
+from shutil import copyfile
+from threading import Thread
+import logging
+
+"""
+--source [-s] picture
+--output [-o]
+"""
+
+parser = argparse.ArgumentParser(description='Sorting folder')
+parser.add_argument("--source", "-s", help="Source folder", required=True)
+parser.add_argument("--output", "-o", help="Output folder", default="dist")
+
+args = vars(parser.parse_args())
+
+source = args.get("source")
+output = args.get("output")
+
+folders = []
+
+
+def grabs_folder(path: Path) -> None:
+    for el in path.iterdir():
+        if el.is_dir():
+            folders.append(el)
+            grabs_folder(el)
+
+
+def copy_file(path: Path) -> None:
+    for el in path.iterdir():
+        if el.is_file():
+            ext = el.suffix
+            new_path = output_folder / ext
+            try:
+                new_path.mkdir(exist_ok=True, parents=True)
+                copyfile(el, new_path / el.name)
+            except OSError as err:
+                logging.error(err)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.ERROR, format="%(threadName)s %(message)s")
+    base_folder = Path(source)
+    output_folder = Path(output)
+    folders.append(base_folder)
+    grabs_folder(base_folder)
+
+    threads = []
+
+    for folder in folders:
+        print(folder)
+        th = Thread(target=copy_file, args=(folder,))
+        th.start()
+        threads.append(th)
+
+    [th.join() for th in threads]
+
+    print('Можно удалять стару папку якщо треба')
+
+>>>>>>> b9847cecd1b3736f22e81a33effd8cd1f7989104
